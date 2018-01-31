6bb89693422b33df0d3fc44d9b8155b574753c9d
Burak KOSE <burakks41@gmail.com>
2018-01-31 23:39:44 +0100

    Rename

--- src/main/scala/akka/stream/alpakka/hdfs/scaladsl/SyncStrategy.scala
+++ src/main/scala/akka/stream/alpakka/hdfs/scaladsl/SyncStrategy.scala
@@ -1,14 +1,14 @@
 package akka.stream.alpakka.hdfs.scaladsl
 
 trait SyncPolicy {
-  def shouldSync(bytes: Array[Byte], offset: Long): Boolean
+  def canSync(bytes: Array[Byte], offset: Long): Boolean
   def reset(): Unit
 }
 
 case class CountSyncPolicy(count: Int) extends SyncPolicy {
   private var executeCount = 0
 
-  override def shouldSync(bytes: Array[Byte], offset: Long): Boolean = {
+  override def canSync(bytes: Array[Byte], offset: Long): Boolean = {
     executeCount += 1
     executeCount >= count
   }
