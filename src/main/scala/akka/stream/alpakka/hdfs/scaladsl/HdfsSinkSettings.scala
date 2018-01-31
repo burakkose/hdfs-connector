dff91055ab4fe27f5e5f24cfec1dd025a25b706d
Burak KOSE <burakks41@gmail.com>
2018-01-31 23:40:29 +0100

    Add settings

--- src/main/scala/akka/stream/alpakka/hdfs/scaladsl/HdfsSinkSettings.scala
+++ src/main/scala/akka/stream/alpakka/hdfs/scaladsl/HdfsSinkSettings.scala
@@ -1,3 +1,17 @@
 package akka.stream.alpakka.hdfs.scaladsl
 
-case class HdfsSinkSettings()
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
+import scala.util.matching.Regex
+
+case class HdfsSinkSettings(
+    overwrite: Boolean = true,
+    bufferSize: Int = 4096,
+    replication: Int = 3,
+    blockSize: Int = 67108864,
+    initialDelay: FiniteDuration = 0.millis,
+    delay: FiniteDuration = 0.millis,
+    pattern: Regex = "*".r,
+    chunkSize: Int = 4096,
+    owner: String
+)
